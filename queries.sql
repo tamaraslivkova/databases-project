91f11d1d2ec1c30bc0655ac3d917a8f0f013a0b3
Tamara Slivková <t.slivkova@student.maastrichtuniversity.nl>
2026-09-17 12:46:51 +0200

    added basic sql operations

--- queries.sql
+++ queries.sql
@@ -1,1 +1,15 @@
+-- adding data
+INSERT INTO Animals (shelter_id, name, age, species, breed, sex)
+VALUES (1,'Max',2,'dog','border collie','male');
 
+-- viewing data
+SELECT * FROM Animals WHERE shelter_id = 1;
+
+-- updating data
+UPDATE Animals
+SET age = 3
+WHERE name = 'Max';
+
+-- removing data
+DELETE FROM Animals
+WHERE name = 'Max';
