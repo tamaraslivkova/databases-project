a5f54c55aa168e65103425135c304fd4b3ec630c
Tamara Slivková <t.slivkova@student.maastrichtuniversity.nl>
2026-09-17 13:23:51 +0200

    added two advanced queries

--- queries.sql
+++ queries.sql
@@ -1,3 +1,4 @@
+-- basic queries
 -- adding data
 INSERT INTO Animals (shelter_id, name, age, species, breed, sex)
 VALUES (1,'Max',2,'dog','border collie','male');
@@ -13,3 +14,25 @@ WHERE name = 'Max';
 -- removing data
 DELETE FROM Animals
 WHERE name = 'Max';
+
+-- advanced queries
+-- finding the workload (number of staff and animals) per shelter
+SELECT 
+    Shelter.name,
+    COUNT(DISTINCT Staff.staff_id) AS total_staff,
+    COUNT(DISTINCT Animals.animal_id) AS total_animals
+FROM Shelter
+LEFT JOIN Staff ON Shelter.shelter_id = Staff.shelter_id
+LEFT JOIN Animals ON Shelter.shelter_id = Animals.shelter_id
+GROUP BY Shelter.name;
+
+-- finding recent animal intakes and with their shelters
+SELECT
+    Animals.name AS animal_name,
+    Animals.species,
+    Shelter.name AS shelter_name,
+    Intake.intake_date
+FROM Animals
+JOIN Shelter ON Animals.shelter_id = Shelter.shelter_id
+JOIN Intake ON Animals.animal_id = Intake.animal_id
+ORDER BY Intake.intake_date DESC;
