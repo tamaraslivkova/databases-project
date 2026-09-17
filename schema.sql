5634f200f925ef14410480c86dfc7ce3cefae400
Tamara Slivková <t.slivkova@student.maastrichtuniversity.nl>
2026-09-17 12:22:16 +0200

    few changes in schema.sql

--- schema.sql
+++ schema.sql
@@ -14,46 +14,47 @@ SET FOREIGN_KEY_CHECKS = 1;
 
 CREATE TABLE Shelter (
     shelter_id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
     address VARCHAR(255) NOT NULL,
-    capacity INT CHECK (capacity >= 0),
     current_capacity INT NOT NULL DEFAULT 0 CHECK (current_capacity >= 0),
     max_capacity INT NOT NULL CHECK (max_capacity >= current_capacity)
-) ENGINE=InnoDB;
+); 
 
 CREATE TABLE Staff (
     staff_id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    role VARCHAR(100) NOT NULL,
     shelter_id INT NOT NULL,
+    first_name VARCHAR(100) NOT NULL,
+    last_name VARCHAR(100) NOT NULL,
+    role VARCHAR(100) NOT NULL,
     FOREIGN KEY (shelter_id) REFERENCES Shelter(shelter_id)
         ON DELETE CASCADE
         ON UPDATE CASCADE
-) ENGINE=InnoDB;
+); 
 
 
 CREATE TABLE Animals (
     animal_id INT AUTO_INCREMENT PRIMARY KEY,
+    shelter_id INT NOT NULL,
+    name VARCHAR(50) NOT NULL,
     age INT CHECK (age >= 0),
     species VARCHAR(100) NOT NULL,
     breed VARCHAR(100),
     sex ENUM('Male', 'Female', 'Unknown'),
-    shelter_id INT NOT NULL,
     FOREIGN KEY (shelter_id) REFERENCES Shelter(shelter_id)
         ON DELETE CASCADE
         ON UPDATE CASCADE
-) ENGINE=InnoDB;
+);
 
 
 CREATE TABLE Intake (
     intake_id INT AUTO_INCREMENT PRIMARY KEY,
-    intake_type VARCHAR(100),
-    date DATE NOT NULL DEFAULT (CURRENT_DATE),
     animal_id INT NOT NULL,
     shelter_id INT NOT NULL,
+    intake_date DATE NOT NULL,
     FOREIGN KEY (animal_id) REFERENCES Animals(animal_id)
         ON DELETE CASCADE
         ON UPDATE CASCADE,
     FOREIGN KEY (shelter_id) REFERENCES Shelter(shelter_id)
         ON DELETE CASCADE
         ON UPDATE CASCADE
-) ENGINE=InnoDB;
+);
